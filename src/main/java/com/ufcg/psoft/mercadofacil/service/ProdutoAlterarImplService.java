5b9c7f81f953ac8fd1969f1e7ffb186ceb731194
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2023-03-27 09:49:32 -0300

    feat: Creating concret class to ProdutoAlterarService

--- src/main/java/com/ufcg/psoft/mercadofacil/service/ProdutoAlterarImplService.java
+++ src/main/java/com/ufcg/psoft/mercadofacil/service/ProdutoAlterarImplService.java
@@ -1,4 +1,18 @@
 package com.ufcg.psoft.mercadofacil.service;
 
-public class ProdutoAlterarImplService {
+import com.ufcg.psoft.mercadofacil.model.Produto;
+import com.ufcg.psoft.mercadofacil.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoAlterarImplService implements  ProdutoAlterarService{
+
+    @Autowired
+    ProdutoRepository<Produto, Long> produtoRepository;
+
+    public Produto alterar(Produto produto) {
+        return this.produtoRepository.update(produto);
+    }
+
 }
