5ee2966760bdb6710f76884e3662b287ed7abc39
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2023-04-08 18:49:03 -0300

    test: criação de testes para o validar codigo de barras service

--- src/test/java/com/ufcg/psoft/mercadofacil/service/ValidarCodigoDeBarrasServiceTest.java
+++ src/test/java/com/ufcg/psoft/mercadofacil/service/ValidarCodigoDeBarrasServiceTest.java
@@ -4,43 +4,55 @@ import com.ufcg.psoft.mercadofacil.model.Produto;
 import com.ufcg.psoft.mercadofacil.repository.ProdutoRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 @AutoConfigureMockMvc
-@DisplayName("Testes para a validacao de um codigo de barras")
+@DisplayName("Testes para a validação de um código de barras")
 public class ValidarCodigoDeBarrasServiceTest {
     @Autowired
-    ValidarCodigoDeBarrarService driver;
+    ValidarCodigoDeBarrasService driver;
+    @Test
+    @DisplayName("Quando o código de barras é válido")
+    public void testCodigoDeBarrasValido() {
 
-    @MockBean
-    ProdutoRepository<Produto, Long> produtoRepository;
-    Produto produto;
+        String codigoDeBarras = "7891234567895";
+        assertTrue(driver.validarCodigoDeBarras(codigoDeBarras));
+    }
 
-    @BeforeEach
-    void setup() {
-        Mockito.when(produtoRepository.find(10L))
-                .thenReturn(Produto.builder()
-                        .id(10L)
-                        .codigoBarra("7899137500104")
-                        .nome("Produto Dez")
-                        .fabricante("Empresa Dez")
-                        .preco(450.00)
-                        .build()
-                );
-        produto = produtoRepository.find(10L);
-        Mockito.when(produtoRepository.update(produto))
-                .thenReturn(Produto.builder()
-                        .id(10L)
-                        .codigoBarra("7899137500104")
-                        .nome("Nome Produto Alterado")
-                        .fabricante("Nome Fabricante Alterado")
-                        .preco(500.00)
-                        .build()
-                );
+    @Test
+    @DisplayName("Quando o código de barras é inválido")
+    public void testCodigoDeBarrasInvalido() {
+        String codigoDeBarras = "7891234567894";
+        assertFalse(driver.validarCodigoDeBarras(codigoDeBarras));
+    }
+
+    @Test
+    @DisplayName("Quando o código de barras tem caracteres invalidos")
+    public void testCodigoDeBarrasComCaracteresInvalidos() {
+        String codigoDeBarras = "78912a456b895";
+        assertFalse(driver.validarCodigoDeBarras(codigoDeBarras));
+    }
+
+    @Test
+    @DisplayName("Quando o código de barras tem tamanho inválido")
+    public void testCodigoDeBarrasComTamanhoInvalido() {
+        String codigoDeBarras = "123456789012";
+        assertFalse(driver.validarCodigoDeBarras(codigoDeBarras));
+    }
+
+    @Test
+    @DisplayName("Quando o código de barras é nulo")
+    public void testCodigoDeBarrasNulo() {
+        String codigoDeBarras = null;
+        assertFalse(driver.validarCodigoDeBarras(codigoDeBarras));
     }
 }
